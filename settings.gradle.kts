c446fc0693252dd0e6b1ecd8be15e2c03abcb26b
Arthur Kazemi <bidadh@gmail.com>
2023-10-27 12:26:38 +1100

    reformat code and modify platform code

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,38 +4,38 @@ include(":androidApp")
 include(":shared")
 
 pluginManagement {
-    repositories {
-        gradlePluginPortal()
-        mavenCentral()
-        google()
-        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
-    }
+  repositories {
+    gradlePluginPortal()
+    mavenCentral()
+    google()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+  }
 
-    plugins {
-        val kotlinVersion = extra["kotlin.version"] as String
-        val agpVersion = extra["agp.version"] as String
-        val composeVersion = extra["compose.version"] as String
+  plugins {
+    val kotlinVersion = extra["kotlin.version"] as String
+    val agpVersion = extra["agp.version"] as String
+    val composeVersion = extra["compose.version"] as String
 
-        kotlin("jvm").version(kotlinVersion)
-        kotlin("multiplatform").version(kotlinVersion)
-        kotlin("android").version(kotlinVersion)
+    kotlin("jvm").version(kotlinVersion)
+    kotlin("multiplatform").version(kotlinVersion)
+    kotlin("android").version(kotlinVersion)
 
-        id("com.android.application").version(agpVersion)
-        id("com.android.library").version(agpVersion)
+    id("com.android.application").version(agpVersion)
+    id("com.android.library").version(agpVersion)
 
-        id("org.jetbrains.compose").version(composeVersion)
-    }
+    id("org.jetbrains.compose").version(composeVersion)
+  }
 }
 
 plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version("0.4.0")
+  id("org.gradle.toolchains.foojay-resolver-convention") version ("0.4.0")
 }
 
 dependencyResolutionManagement {
-    @Suppress("UnstableApiUsage")
-    repositories {
-        mavenCentral()
-        google()
-        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
-    }
+  @Suppress("UnstableApiUsage")
+  repositories {
+    mavenCentral()
+    google()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+  }
 }
--- shared/src/androidMain/kotlin/main.android.kt
+++ shared/src/androidMain/kotlin/main.android.kt
@@ -1,5 +1,7 @@
+import android.os.Build
 import androidx.compose.runtime.Composable
 
-actual fun getPlatformName(): String = "Android"
+actual fun getPlatformName(): String = Build.BRAND + " " + Build.MODEL
 
-@Composable fun MainView() = App()
+@Composable
+fun MainView() = App()
