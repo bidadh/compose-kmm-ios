fc43707342459330edc40e835b397fa3540452f2
Arthur Kazemi <bidadh@gmail.com>
2023-10-27 13:50:15 +1100

    upgrade android gradle plugin to 8.2.0-rc01 and minor changes

--- shared/src/androidMain/kotlin/main.android.kt
+++ shared/src/androidMain/kotlin/main.android.kt
@@ -2,7 +2,6 @@ import android.os.Build
 import androidx.compose.runtime.Composable
 
 actual fun getPlatformName(): String = Build.MANUFACTURER +
-    " " + Build.DISPLAY +
     " " + Build.VERSION.RELEASE
 
 @Composable
